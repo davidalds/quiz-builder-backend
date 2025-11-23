9ed0ae1998d1577baa2dfe72a14fa3cee276a3f6
davidalds <david57araujo@gmail.com>
2025-11-23 20:19:12 -0300

    change main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 
-const allowedOrigins = process.env.DOMAINS?.split(',').map((o) => o.trim())
+const allowedOrigins = (process.env.DOMAINS || '')
+  .split(',')
+  .filter((origin) => origin.trim() !== '')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -13,22 +15,9 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   )
-  // console.log("DOMAINS RAW =", process.env.DOMAINS);
-  // console.log("DOMAINS SPLIT =", process.env.DOMAINS?.split(','));
+  console.log('CORS Allowed Origins:', allowedOrigins)
   app.enableCors({
-    origin: (
-      origin: string | undefined,
-      callback: (err: Error | null, allow?: boolean) => void,
-    ) => {
-      // Permite requisições sem origin (ex.: Postman)
-      if (!origin) return callback(null, true)
-
-      if (allowedOrigins?.includes(origin)) {
-        return callback(null, true)
-      }
-
-      return callback(new Error(`Origin not allowed by CORS: ${origin}`), false)
-    },
+    origin: allowedOrigins,
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   })
