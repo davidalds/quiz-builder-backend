d17dcf6be074bf848d307d8eddd5c1985e16a098
davidalds <121527954+davidalds@users.noreply.github.com>
2025-11-23 00:54:27 -0300

    Refactor CORS configuration to use callback

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,16 @@ async function bootstrap() {
   console.log("DOMAINS RAW =", process.env.DOMAINS);
   console.log("DOMAINS SPLIT =", process.env.DOMAINS?.split(','));
   app.enableCors({
-    origin: process.env.DOMAINS?.split(','),
+  origin: (origin, callback) => {
+    const allowed = process.env.DOMAINS?.split(',').map(d => d.trim());
+    if (!origin || allowed.includes(origin)) {
+      return callback(null, true);
+    }
+      console.log("CORS BLOQUEADO:", origin);
+      return callback(new Error('Not allowed by CORS'));
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  })
+  });
   await app.listen(process.env.PORT ?? 8080)
 }
 bootstrap()
